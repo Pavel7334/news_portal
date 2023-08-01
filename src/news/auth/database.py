b73f87ef53038485705ba1b3ae7f70407d8c1eca
Игонин Павел <88630513+Pavel7334@users.noreply.github.com>
2023-08-01 16:05:41 +0400

    Удалил Base, поправил импорты

--- src/news/auth/database.py
+++ src/news/auth/database.py
@@ -1,16 +1,14 @@
 from typing import AsyncGenerator
 
 from fastapi import Depends
-from fastapi_users.db import SQLAlchemyBaseUserTable, SQLAlchemyUserDatabase
+from fastapi_users.db import SQLAlchemyUserDatabase
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
-from sqlalchemy.ext.declarative import DeclarativeMeta, declarative_base
 from sqlalchemy.orm import sessionmaker
 
 from config import DB_HOST, DB_NAME, DB_PASS, DB_PORT, DB_USER
 from src.news.models.models import User
 
 DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-Base: DeclarativeMeta = declarative_base()
 
 
 engine = create_async_engine(DATABASE_URL)
